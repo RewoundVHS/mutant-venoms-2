98a6e1ffec7d974c5cd7ed7120dd0f93554b6a9c
Neve Laughery <nevelaughery@gmail.com>
2018-03-19 14:44:38 -0400

    First commit

---
+++ player2.cpp
@@ -0,0 +1,418 @@
+// Neve Laughery
+// CSCI330
+
+#include "player2.h"
+
+const string PlayerClass::DEFAULT_NAME = "DEFAULT"; 
+const PlayerClass::PlayerType PlayerClass::DEFAULT_TYPE = PlayerClass::HUNTER;
+
+const int PlayerClass::STATS[MAX_STATS][MAX_TYPES] = {
+//   Hu  Sc  Fr  Sp  Vi
+    {43, 82, 61, 51, 21},    // Max health
+    {43, 82, 61, 51, 21},    // Health
+    { 8, 10, 10,  9,  8},    // Willpower
+    { 9,  6,  9, 16,  3},    // Max speed
+    { 9,  6,  9, 16,  3},    // Current speed
+    { 9,  6,  9, 16,  3},    // Momentum
+    { 7, 10, 11, 12,  6},    // Power
+    { 1,  3,  3,  2,  2},    // Physical damage rolls
+    { 6,  6,  6,  6,  6},    // Faces on damage roll dice
+    { 1,  9,  9,  8, 11},    // Defense value
+    {-1, -1, -1, -1, -1},    // Row
+    {-1, -1, -1, -1, -1},    // Col
+    { 0,  0,  0,  0,  0},    // Active
+    { 0,  0,  0,  0,  0}};   // Direction
+
+const int PlayerClass::IMPACT[MAX_2D6][MAX_PWR] = {   
+//                  Power Rating
+//    12  11  10   9   8   7   6   5   4   3   2    Die Roll
+    {  w,  w,  w,  w,  w,  w,  w,  w,  w,  w, 10},  // 12
+    {  w,  w,  w,  w,  w,  w,  w,  w,  w, 10,  9},  // 11
+    {  w,  w,  w,  w,  w,  w,  w,  w, 10,  9,  8},  // 10
+    {  w,  w,  w,  w,  w,  w,  w, 10,  9,  8,  7},  // 9
+    {  w,  w,  w,  w,  w,  w, 10,  9,  8,  7,  6},  // 8
+    {  w,  w,  w,  w,  w, 10,  9,  8,  7,  6,  5},  // 7
+    {  w,  w,  w,  w, 10,  9,  8,  7,  6,  5,  4},  // 6
+    {  w,  w,  w, 10,  9,  8,  7,  6,  5,  4,  3},  // 5
+    {  w,  w, 10,  9,  8,  7,  6,  5,  4,  3,  2},  // 4
+    {  w, 10,  9,  8,  7,  6,  5,  4,  3,  2,  1},  // 3
+    { 10,  9,  8,  7,  6,  5,  4,  3,  2,  1,  0}}; // 2
+
+const int PlayerClass::CRITICAL_WOUND[MAX_2D6][MAX_WILL] = {
+//                  Willpower Rating           
+//    2   3   4   5   6   7   8   9  10  11  12    Die Roll
+    {-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1},  // 2
+    { k, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1},  // 3
+    { k,  k, -1, -1, -1, -1, -1, -1, -1, -1, -1},  // 4
+    { k,  k,  k, -1, -1, -1, -1, -1, -1, -1, -1},  // 5
+    { k,  k,  k,  k, -1, -1, -1, -1, -1, -1, -1},  // 6
+    { k,  k,  k,  k,  k, -1, -1, -1, -1, -1, -1},  // 7 
+    { k,  k,  k,  k,  k,  k, -1, -1, -1, -1, -1},  // 8  
+    { k,  k,  k,  k,  k,  k,  k, -1, -1, -1, -1},  // 9  
+    { k,  k,  k,  k,  k,  k,  k,  k, -1, -1, -1},  // 10 
+    { k,  k,  k,  k,  k,  k,  k,  k,  k, -1, -1},  // 11
+    { k,  k,  k,  k,  k,  k,  k,  k,  k,  k, -1}}; // 12
+
+PlayerClass::PlayerClass(string initName, PlayerType initType) {
+    if (initName.size() <= 0)
+        name = DEFAULT_NAME;
+    else
+        name = initName;
+
+    if (initType >= HUNTER && initType < MAX_TYPES)
+        type = initType;
+    else
+        type = DEFAULT_TYPE;
+
+    for (int i=0; i<MAX_STATS; i++) {
+        playerStats[i] = STATS[i][type];
+    }
+}
+
+string PlayerClass::Name() const {
+    return name;
+}
+
+int PlayerClass::Type() const {
+    return type;
+}
+
+string PlayerClass::TypeStr() const {
+    string typeString;
+
+    switch (type) {
+        case HUNTER:
+            typeString = "HUNTER";
+            break;
+        case SCAVENGER:
+            typeString  = "SCAVENGER";
+            break;
+        case FROG:
+            typeString = "FROG";
+            break;
+        case SPIDER:
+            typeString = "SPIDER";
+            break;
+        case VIPER:
+            typeString = "VIPER";
+            break;
+    }
+    return typeString;
+}
+
+int PlayerClass::Row() const{
+    return playerStats[ROW];
+}
+
+int PlayerClass::Col() const {
+    return playerStats[COL];
+}
+
+char PlayerClass::Dir() const {
+    return GameSpace::DIR_CHAR[playerStats[DIR]];  
+}
+
+int PlayerClass::Health() const {
+    return playerStats[HEALTH];
+}
+
+int PlayerClass::WillPower() const {
+    return playerStats[WILLPOWER];
+}
+
+int PlayerClass::MaxSpeed() const {
+    return playerStats[MAX_SPEED];
+}
+
+int PlayerClass::CurrentSpeed() const {
+    return playerStats[CUR_SPEED];
+}
+
+int PlayerClass::Momentum() const {
+    return playerStats[MOMENTUM];
+}
+
+int PlayerClass::Power() const {
+    return playerStats[POWER];
+}
+
+int PlayerClass::RollPhysDam() const {
+    return Dice::Roll(name, GameSpace::DAMAGE, playerStats[DAMAGE_ROLLS], playerStats[ROLL_FACES]);
+}
+
+int PlayerClass::Defense() const {
+    return playerStats[DEFENSE];
+}
+
+bool PlayerClass::Human() const {  
+    bool isHuman;
+    switch (type) {
+        case HUNTER:
+            isHuman = true;
+            break;
+        case SCAVENGER:
+            isHuman = true;
+            break;
+        default:
+            isHuman = false;
+            break;
+    }
+    return isHuman;
+}
+
+bool PlayerClass::Venom() const {  
+    bool isVenom;
+    switch (type) {
+        case FROG:
+            isVenom = true;
+            break;
+        case SPIDER:
+            isVenom = true;
+            break;
+        case VIPER:
+            isVenom = true;
+            break;
+        default:
+            isVenom = false;
+            break;
+    }
+    return isVenom;
+}
+
+bool PlayerClass::IsAlive() const {
+    return (playerStats[HEALTH] > 0 || playerStats[WILLPOWER] > 0);
+}
+
+bool PlayerClass::IsDead() const {
+    return (playerStats[HEALTH] <= 0 || playerStats[WILLPOWER] <= 0);
+}
+
+bool PlayerClass::IsActive() const { 
+    return (playerStats[ACTIVE] == 1); 
+}
+
+bool PlayerClass::IsMyEnemy(PlayerClass* p) const {
+    bool isEnemy = false;
+    if (p != NULL) 
+        isEnemy = ((Human()) && !(p->Human()) || (Venom() && (type != p->Type())));
+
+    return (isEnemy);
+}
+
+bool PlayerClass::MakeActive() {
+    bool set = false;
+
+    if (IsAlive()) {
+        playerStats[ACTIVE] = 1;
+        set = true;
+    }
+    return set;
+}
+
+bool PlayerClass::MakeInActive() {
+    playerStats[ACTIVE] = 0;
+    return true;
+}
+
+bool PlayerClass::MakeDead() {
+    if (IsActive() || IsDead()) {
+        playerStats[HEALTH] = 0;
+        playerStats[WILLPOWER] = 0;
+        playerStats[CUR_SPEED] = 0;
+        playerStats[MOMENTUM] = 0;
+        playerStats[ACTIVE] = 0;
+    }
+    return IsDead();
+}
+
+int PlayerClass::Wounded(int damage) {
+    if (IsActive() && damage >= 0) 
+        playerStats[HEALTH] -= damage;
+
+    if (IsDead())
+        MakeDead();
+
+    return playerStats[HEALTH];
+}
+
+int PlayerClass::Invigorate(int vigor) {
+    if ((IsActive()) && (vigor >= 0)) {
+        playerStats[HEALTH] += vigor;
+        if (playerStats[HEALTH] > playerStats[MAX_HEALTH])
+            playerStats[HEALTH] = playerStats[MAX_HEALTH];
+    }
+    return playerStats[HEALTH];
+}
+
+bool PlayerClass::Heal() {
+    int healAmount = 10;
+    bool healed = false;
+
+    if (IsActive() && IsAlive()) {
+        playerStats[CUR_SPEED] = playerStats[MAX_SPEED];
+        playerStats[HEALTH] += healAmount;
+        if (playerStats[HEALTH] > playerStats[MAX_HEALTH])
+            playerStats[HEALTH] = playerStats[MAX_HEALTH];
+        healed = true;
+    }
+    return healed;
+}
+
+int PlayerClass::WillDamage(int willDmg) {
+    if (IsActive() && (willDmg >= 0))
+        playerStats[WILLPOWER] -= willDmg;
+
+    if (playerStats[WILLPOWER] <= 0)
+        MakeDead();
+}
+
+int PlayerClass::AddWill(int addWill) {
+    if (IsActive() && (addWill >= 0))
+        playerStats[WILLPOWER] += addWill;
+
+    return playerStats[WILLPOWER];
+}
+
+int PlayerClass::SlowDown(int hazard) {
+    if (IsActive() && hazard >= 0) {
+        playerStats[CUR_SPEED] -= hazard;
+        if (playerStats[CUR_SPEED] < 0)
+            playerStats[CUR_SPEED] = 0;
+        if (playerStats[MOMENTUM] > playerStats[CUR_SPEED])
+            ResetMomentum();
+    }
+    return playerStats[CUR_SPEED];
+}
+
+int PlayerClass::ResetCurrentSpeed() {
+    if (IsActive()) 
+        playerStats[CUR_SPEED] = playerStats[MAX_SPEED];
+    
+    return playerStats[CUR_SPEED];
+}
+
+int PlayerClass::ResetMomentum() {
+    if (IsActive()) 
+        playerStats[MOMENTUM] = playerStats[CUR_SPEED];
+
+    return playerStats[MOMENTUM];
+}
+
+int PlayerClass::UseMomentum(int num) {
+    if (IsActive() && num >= 0) {
+        playerStats[MOMENTUM] -= num;
+        if (playerStats[MOMENTUM] < 0) 
+            playerStats[MOMENTUM] = 0; 
+    }
+    return playerStats[MOMENTUM];
+}
+
+bool PlayerClass::SetCell(int newRow, int newCol) {
+    bool set = false;
+
+    if ((newRow >= 0 && newCol >= 0) || (newRow == -1 && newCol == -1)) {
+        playerStats[ROW] = newRow;
+        playerStats[COL] = newCol;
+        set = true;
+    }
+    return set;
+}
+
+bool PlayerClass::SetDir(DirType newDir) {
+    bool set = false;
+    if (newDir >= GameSpace::NORTH && newDir <= GameSpace::WEST) {
+        playerStats[DIR] = newDir;
+        set = true;
+    }
+    return set;
+}
+
+// Needs modified to include weapon data
+void PlayerClass::Write(ostream& out) const {
+    out << TypeStr() << '#';
+    if (IsActive())
+        out << '1';
+    else
+        out << '0';
+    out << '#' << Name() << '#' << '(' << Row() << ',' << Col() << ',' << Dir()
+    << ")#" << Health() << '#' << WillPower() << '#' << Power() << '#' 
+    << CurrentSpeed() << '#' << Momentum() << '#' << playerStats[DAMAGE_ROLLS] 
+    << 'D' << playerStats[ROLL_FACES] << '#' << Defense() << '#' << endl;
+}
+
+// Might need modified along with Write
+ostream& operator<<(ostream& out, const PlayerClass *p) {
+    if (p != NULL)
+        p->Write(out);
+    return out;
+}
+
+int PlayerClass::UpdateDefense(int amount) {
+    if (IsActive())
+        playerStats[DEFENSE] += amount;
+    if(playerStats[DEFENSE] < 0);
+        playerStats[DEFENSE] = 0;
+
+    return playerStats[DEFENSE];
+}
+
+int PlayerClass::HitScore() {
+    return Dice::Roll(name, GameSpace::HITSCORE, 2, 6); 
+}
+
+int PlayerClass::Impact() {
+
+}
+
+bool PlayerClass::CriticalWound() {
+    int damage =10;
+    int willDmg = 1;
+    int roll;
+    int slowed = playerStats[CUR_SPEED] - playerStats[CUR_SPEED]/2 ;
+    int crit;
+
+    if(IsActive()) {
+        Wounded(damage);
+        SlowDown(slowed);
+        if (IsAlive()) {
+            // Is it appropriate to have the floating 2 and 6 here?
+            roll = Dice::Roll(name, GameSpace::WOUND, 2, 6);
+            crit = CRITICAL_WOUND[roll][playerStats[WILLPOWER]];
+
+            if (crit = -1)
+                WillDamage(willDmg);
+            else
+                MakeDead();
+        }
+    }
+    return IsAlive();
+}
+
+Weapon* PlayerClass::DropWeapon() {
+    Weapon* dropped;
+
+    if (IsActive() && HasWeapon()) {
+        dropped = playerWeapon;
+        playerWeapon = NULL;
+    } else {
+        dropped = NULL;
+    }
+    return dropped;
+}
+
+bool PlayerClass::HasWeapon() {
+    return(playerWeapon != NULL);
+}
+
+Weapon* PlayerClass::GrabWeapon(Weapon* weapon) {
+    Weapon* oldWeapon = playerWeapon;
+
+    if (IsActive() && Human())
+        playerWeapon = weapon;
+    // If player has skill with weapon, skill is used
+    // Otherwise weapon skill is set to 1
+}
+
+bool PlayerClass::ImprovedSkill(int improvement) {    
+
+}
---
+++ player2.h
@@ -0,0 +1,357 @@
+// Neve Laughery
+// CSCI330
+
+#ifndef _PLAYER
+#define _PLAYER
+
+#include <string>
+#include <iostream>
+#include "gamespace.h"
+#include "dice.h"
+#include "wpn.h"
+
+using namespace std;
+using namespace GameSpace;
+
+class PlayerClass {
+
+    public:
+        enum PlayerType {
+            HUNTER,
+            SCAVENGER,
+            FROG,
+            SPIDER,
+            VIPER
+        };
+
+        enum StatType {
+            MAX_HEALTH,
+            HEALTH,
+            WILLPOWER,
+            MAX_SPEED,
+            CUR_SPEED,
+            MOMENTUM,
+            POWER,
+            DAMAGE_ROLLS,
+            ROLL_FACES,
+            DEFENSE,
+            ROW,
+            COL,
+            ACTIVE,
+            DIR
+        };
+
+    private: 
+        static const string DEFAULT_NAME; 
+        static const PlayerType DEFAULT_TYPE;
+        static const int MAX_TYPES = 5;
+        static const int MAX_STATS = 14;
+        static const int DEFAULT_POS = -1;
+        static const int MAX_2D6 = 11;
+        static const int MAX_PWR = 11;
+        static const int MAX_WILL = 11;
+        static const int w = -1;
+        static const int k = 0;
+        static const int STATS[MAX_STATS][MAX_TYPES];
+        static const int IMPACT[MAX_2D6][MAX_PWR];  
+        static const int CRITICAL_WOUND[MAX_2D6][MAX_WILL];
+
+    public:
+        /*
+        Narrative: Initializes an instance of the player class
+        */
+        PlayerClass(string initName = DEFAULT_NAME, PlayerType initType = DEFAULT_TYPE);
+
+        /*
+        Narrative: Returns the player's name
+        Pre-condition: Player's name has been initialized
+        Post-condition: Player's name has been returned
+        */
+        string Name() const;
+        
+        /*
+        Narrative: Returns the player's type
+        Pre-condition: Player's type is within boundaries
+        Post-condition: Player's type has been returned  
+        */
+        int Type() const;
+        
+        /*
+        Narrative: Returns the player's type as a string
+        Pre-condition: Player's type has been initialized
+        Post-condition: Type string has been returned
+        */
+        string TypeStr() const;
+        
+        /*
+        Narrative: Returns the player's cell row
+        Pre-condition: Player's cell row has been initialized
+        Post-condition: Players's cell row has been returned
+        */
+        int Row() const;
+        
+        /*
+        Narrative: Returns the player's cell col
+        Pre-condition: Player's cell col has been initialized
+        Post-condition: Players's cell col has been returned
+        */
+        int Col() const;
+        
+        /*
+        Narrative: Returns the associated character for a DirType
+        Pre-condition: Dir is within boundaries
+        Post-condition: Character has been returned
+        */
+        char Dir() const;
+        
+        /*
+        Narrative: Returns the player's health
+        Pre-condition: Player's health has been initialized
+        Post-condition: Player's health has been returned
+        */
+        int Health() const;
+        
+        /*
+        Narrative: Returns the player's willpower
+        Pre-condition: Player's willpower has been initialized
+        Post-condition: Player's willpower has been returned
+        */
+        int WillPower() const;
+        
+        /*
+        Narrative: Returns the player's maximum speed 
+        Pre-condition: Player's maximum speed has been initialized
+        Post-condition: Player's speed has been returned
+        */
+        int MaxSpeed() const;
+        
+        /*
+        Narrative: Returns the player's current speed
+        Pre-condition: Player's current speed has been initialized
+        Post-condition: Player's current speed has been returned 
+        */
+        int CurrentSpeed() const;
+        
+        /*
+        Narrative: Returns the player's momentum
+        Pre-condition: Player's momentum has been initialized
+        Post-condition: Player's momentum has been returned
+        */
+        int Momentum() const;
+        
+        /*
+        Narrative: Returns the player's power
+        Pre-condition: Player's power has been initialized
+        Post-condition: Player's power has been returned 
+        */
+        int Power() const;
+        
+        /*
+        Narrative: Returns the player's rolled physical damage
+        Pre-condition: Player's rolled physical damage has been initialized
+        Post-condition: Player's rolled physical damage has been returned  
+        */
+        int RollPhysDam() const;
+        
+        /*
+        Narrative: Returns the player's defense value 
+        Pre-condition: Player's defense value has been initialized
+        Post-condition: Player's defense value has been returned 
+        */
+        int Defense() const;
+        
+        /*
+        Narrative: Returns whether or not the player is a human
+        Pre-condition: Human bool has been initialized
+        Post-condition: Human bool has been returned 
+        */
+        bool Human() const;
+        
+        /*
+        Narrative: Returns whether or not the player is a venom
+        Pre-condition: Venom bool has been initialized
+        Post-condition: Venom bool has been returned 
+        */
+        bool Venom() const;
+        
+        /*
+        Narrative: Returns whether or not the player is alive
+        Pre-condition: Alive bool has been initialized
+        Post-condition: Alive bool has been returned
+        */
+        bool IsAlive() const;
+        
+        /*
+        Narrative: Returns whether or not the player is dead
+        Pre-condition: Alive bool has been initialized
+        Post-condition: Opposite of the alive bool has been returned 
+        */
+        bool IsDead() const;
+        
+        /*
+        Narrative: Returns whether or not the player is active
+        Pre-condition: Active bool has been initialized
+        Post-condition: Active bool has been returned 
+        */
+        bool IsActive() const;
+        
+        /*
+        Narrative: Determines if player p is an enemy. P is an enemy if the 
+        player is human and enemy is a venom or if the player is a venom and
+        the enemy is any other type but their own.
+        Pre-condition: P is not null
+        Post-condition: Returns true if player p is an enemy and false if it is
+        not.
+        */
+        bool IsMyEnemy(PlayerClass* p) const;
+        
+        /*
+        Narrative: Sets the player's state to active if the player is alive
+        Pre-condition: None
+        Post-condition: Returns true if the function was able to set the state 
+        to active and false if it could not
+        */
+        bool MakeActive();
+        
+        /*
+        Narrative: Sets the player's status to inactive and returns true
+        Pre-condition: None
+        Post-condition: Player's status has been set to inactive and true has 
+        been returned 
+        */
+        bool MakeInActive();
+        
+        /*
+        Narrative: Sets all of the appropriate values to indicate the player 
+        has died
+        Pre-condition: None
+        Post-condition: Health, willpower, current speed, and active have been 
+        set to 0 then returns true 
+        */
+        bool MakeDead();
+        
+        /*
+        Narrative: Applies the damage if the player is active and the damage is 
+        greater than or equal to zero
+        Pre-condition: None
+        Post-condition: Damage has been applied if player is active and the 
+        damage is greater than or equal to zero 
+        */
+        int Wounded(int damage);
+        
+        /*
+        Narrative: If the player is active and vigor amount is greater than or 
+        equal to zero the player is healed
+        Pre-condition: None
+        Post-condition: If the player is active it is healed for vigor amount 
+        and health is returned
+        */
+        int Invigorate(int vigor);
+        
+        /*
+        Narrative: If the player is active and alive the player's speed is set 
+        to maximum speed and the player is healed
+        Pre-condition: None
+        Post-condition: If the player is active and alive, the player is healed 
+        and gains maximum speed 
+        */
+        bool Heal();
+        
+        int WillDamage(int willDmg);
+
+        int AddWill(int addWill);
+
+        /*
+        Narrative: Detracts an amount from the player's current speed if they are active
+        Pre-condition: None
+        Post-condition: Hazard has been applied, if the player's momentum is less 
+        than their speed their momentum is set equal to current speed. Current 
+        speed is returned.
+        */
+        int SlowDown(int hazard);
+        
+        /*
+        Narrative: If the player is active their current speed is set to maximum
+        speed
+        Pre-condition: None
+        Post-condition: If the player is active their current speed has been 
+        reset to maximum speed 
+        */
+        int ResetCurrentSpeed();
+        
+        /*
+        Narrative: If the player is active their momentum is set to their current speed
+        Pre-condition: None
+        Post-condition: If the player is active their momentum has been set to 
+        their current speed 
+        */
+        int ResetMomentum();
+        
+        /*
+        Narrative: If the player is active and num is greater than or equal to 
+        0 num is subtracted from their momentum
+        Pre-condition: None
+        Post-condition: If the player is active num has been removed from their 
+        momentum 
+        */
+        int UseMomentum(int num);
+        
+        /*
+        Narrative: Sets the cell that the player will now reside in
+        Pre-condition: None
+        Post-condition: Checks to see if the new coordinates are valid. If they
+        are, sets them and returns true, otherwise returns false. 
+        */
+        bool SetCell(int newRow, int newCol);
+        
+        /*
+        Narrative: Sets the player's new direction
+        Pre-condition: None
+        Post-condition: New direction has been set and true has been returned
+        */
+        bool SetDir(DirType newDir);
+        
+        int UpdateDefense(int amount);
+
+        int HitScore();
+
+        int Impact();
+
+        bool CriticalWound();
+
+        Weapon* DropWeapon();
+
+        bool HasWeapon();
+
+        Weapon* GrabWeapon(Weapon* weapon);
+
+        bool ImprovedSkill(int improvement);
+
+        /*
+        Narrative: Adds the player's statistics in a human readable format to a stream
+        Pre-condition: None
+        Post-condition: Player's statistics have been returned 
+        */
+        void Write(ostream& out) const;
+
+    private:
+        // Array of all of the stats a player has
+        int playerStats[MAX_STATS];     
+
+        string name;    // Player's name
+        int type;       // Player's type
+        DirType dir;    // Player's direction
+        bool human;     // Whether or not the player is a human
+        bool venom;     // Whether or not the player is a venom
+        Weapon* playerWeapon;
+
+};
+
+/*
+Narrative: Adds a player's statistics stream to a stream
+Pre-condition: None
+Post-condition: Statistics have been added to the stream and returned
+*/
+ostream& operator<<(ostream& out, const PlayerClass *p);
+
+#endif
